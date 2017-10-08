63b09de0c0da7edf0ac8c127e57b84addd226054
Thomas Grivet <dbrazzor@tutanota.com>
2017-10-08 16:53:31 +0200

    :sparkles: Sockets !

--- src/components/live_chat/index.jsx
+++ src/components/live_chat/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import openSocket from 'socket.io-client';
+
 import NavigationBar from '../smallviews/nav_bar';
 import SidePanel from './smallviews/side_panel'
 import Content from './smallviews/content';
@@ -9,25 +11,41 @@ import WaitingForServer from './waiting_for_server';
 
 import '../../styles/live_chat/index.css';
 
+let socket = null;
+
 class LiveChat extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			isConnected: false
+			connectionStatus: 'waiting'
 		}
 	}
 
+	componentWillMount() {
+		socket = openSocket('localhost:8000');
+		socket.on('connect', () => this.setState({ connectionStatus: 'connected' }));
+		socket.on('connect_error', () => this.setState({ connectionStatus: 'error' }));
+		socket.on('disconnect', () => this.setState({ connectionStatus: 'disconnected' }));
+	}
+
 	render() {
-		const isConnected = this.state.isConnected;
-		if (isConnected) {
+		const connectionStatus = this.state.connectionStatus;
+		if (connectionStatus === 'connected') {
 			return <Connected />
+		} else if (connectionStatus === 'waiting') {
+			return <WaitingForServer />
 		}
-		return <WaitingForServer />
+		return (
+			<div>
+				{'error'}
+			</div>
+		)
 	}
 }
 
 const Connected = () => (
 	<div id="live-chat-container">
+		<NavigationBar />
 		<SidePanel />
 		<Content />
 		<Inputs />
--- src/components/live_chat/waiting_for_server.jsx
+++ src/components/live_chat/waiting_for_server.jsx
@@ -9,7 +9,7 @@ class WaitingForServer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			elapsedTime: 0,
+			elapsedTime: 1,
 			open: false
 		}
 	}
