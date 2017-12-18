2f3b1f0dcf8b3ef2e1e2e9afba7e8509931d38f7
Joel <princejoel@hotmail.fr>
2017-12-18 10:11:22 +0100

    created login page

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,16 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import './login.css';
+import Button from 'material-ui/Button';
+import usericon from './usericon.png'
+import { loginUser as loginUserAction } from '../../actions/user_actions';
+import TextField from "material-ui/TextField";
 
 class Login extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			changeThisKeyWow: 'oui'
+			username: null,
+			password: null
 		}
 	}
 
 	render() {
-		return 'Hey edit moi stp'
+		const { username, password } = this.state;
+		const { loginUser } = this.props;
+
+		return (
+      <div id='belly'>
+        <div id="login-rectangle">
+          <img id="imageUser" src={usericon} />
+          <div id="form">
+            <TextField
+            label="Username"
+            onChange={e => this.setState({ username: e.target.value })}
+            margin="normal"
+
+            />
+          <br></br>
+
+          <TextField
+            label="Password"
+            hintText="Password Field"
+            floatingLabelText="Password"
+            onChange={e => this.setState({ password: e.target.value })}
+            type="password"
+            
+          />
+          </div>
+        </div>
+    </div>
+
+
+
+		);
 	}
 }
 
-export default Login;
+
+
+export default (Login);
